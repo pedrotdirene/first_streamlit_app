689d3ce6afd6a583ba3a559175cf3c34558c3309
Pedro Henrique Tadra Direne <104918184+pedrotdirene@users.noreply.github.com>
2022-05-04 13:46:31 -0300

    Update streamlit_app.py
    changing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthly diner fish')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
